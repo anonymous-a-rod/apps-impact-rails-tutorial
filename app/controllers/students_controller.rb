fbf9f2c59a9131bf8545dc013d6ccc8ff16b1f81
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2023-04-25 17:12:22 -0400

    35 create form

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,5 +8,20 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :email)
+  end
+
 
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -7,5 +7,20 @@ class TeachersController < ApplicationController
   def new
     @teacher = Teacher.new
   end
-  
+
+  def create
+    @teacher = Teacher.new(teacher_params)
+    if @teacher.save
+      redirect_to teachers_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def teacher_params
+    params.require(:teacher).permit(:first_name, :last_name, :biography)
+  end
+
 end
