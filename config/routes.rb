72ad4f14a175f0fd1a2c3c4594b3b3c9aaddfcd2
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2023-04-25 15:46:11 -0400

    33 listing resource data on browser

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "welcome#index"
 
-  resources :students
+  resources :students, :teachers
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,17 @@ student = Student.create(
   Student.create(
     first_name: "Student #{i+1}",
     last_name: "Lname #{i+1}",
-    email: "{i}@gmail.com"
+    email: "#{i}@gmail.com"
   )
 
+end
+
+Teacher.destroy_all
+
+20.times do |i|
+  Teacher.create(
+    first_name: "Teacher #{i}",
+    last_name: "Lname #{i}",
+    biography: "blah blah #{i}"
+  )
 end
