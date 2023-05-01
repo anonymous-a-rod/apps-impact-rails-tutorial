6b9417dafb87ee1f17c7177a863af0e771851438
Aaron <32913725+anonymous-a-rod@users.noreply.github.com>
2023-05-01 14:13:07 -0400

    validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,9 @@ class Student < ApplicationRecord
   has_many :student_projects
   has_many :projects, through: :student_projects
   validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
+  validates :first_name, :last_name, length: { minimum: 2, maximum: 50}
+  validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/, message: 'only letters are allowed'}
+
+
 end
